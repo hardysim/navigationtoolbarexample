6697d5f4f7b1f45be49ec7bc833549abb16fbf14
Hardt, Simon <simon.hardt@loewen.de>
2021-01-12 14:08:19 +0100

    add navigation

---
+++ app/src/main/java/com/example/navigation_toolbar_example/BlankFragmentA.kt
@@ -0,0 +1,27 @@
+package com.example.navigation_toolbar_example
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class BlankFragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blank_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button)?.setOnClickListener {
+            findNavController().navigate(BlankFragmentADirections.actionBlankFragmentAToBlankFragmentB())
+        }
+    }
+}
--- app/src/main/java/com/example/navigation_toolbar_example/MainActivity.kt
+++ app/src/main/java/com/example/navigation_toolbar_example/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.navigation_toolbar_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Toolbar>(R.id.toolbar)?.apply {
+            setSupportActionBar(this)
+            setupWithNavController(this, findNavController(R.id.nav_host_fragment))
+        }
     }
 }
